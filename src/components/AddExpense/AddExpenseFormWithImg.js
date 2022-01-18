479440bba90d9e1e8f5847253b52c79b65f3b8c9
Parth Cheulkar <pscheulkar12@gmail.com>
2022-01-18 12:11:32 +0530

    add expense form

--- src/components/AddExpense/AddExpenseFormWithImg.js
+++ src/components/AddExpense/AddExpenseFormWithImg.js
@@ -4,7 +4,7 @@ import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 const SERVER_BASE_URL = process.env.REACT_APP_SERVER_BASE_URL;
-let base64 = require('base-64');
+let base64 = require("base-64");
 export default function AddExpenseFormWithImg(props) {
   let history = useNavigate();
   const nano_id = "9f4d89f1-66cd-4db6-94fe-90d22fa3048c";
@@ -23,7 +23,7 @@ export default function AddExpenseFormWithImg(props) {
         method: "POST",
         headers: {
           "Content-Type": "multipart/form-data",
-          "Authorization": `Basic ${base64.encode(process.env.NANO_API_KEY)}`,
+          'Authorization': `Basic ${base64.encode(process.env.NANO_API_KEY)}`,
         },
         body: {
           modelId: nano_id,
@@ -32,7 +32,7 @@ export default function AddExpenseFormWithImg(props) {
       }
     )
       .then((response) => {
-        response.json();
+        console.log(response.json());
       })
       .then((data) => {
         console.log("Success : ", data);
@@ -40,49 +40,52 @@ export default function AddExpenseFormWithImg(props) {
       .then((error) => {
         console.log("Error", error);
       });
-      console.log("Nano : " ,nano_response);
+    console.log("Nano : ", nano_response);
   };
 
-   const backendOCRExtractor = async () => {
-     const ocr_response = await fetch(
-       "https://cash-flowapp.herokuapp.com/api/detect-expenditure/",
-       {
-         //  mode: "no-cors",
-         method: "POST",
-         headers: {
-           "Content-Type": "multipart/form-data",
-           Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-         },
-         body: {
-           exp_pic: uploadFile[0],
-         },
-       }
-     )
-       .then((response) => {
-         response.json();
-       })
-       .then((data) => {
-         console.log("Success : ", data);
-       })
-       .then((error) => {
-         console.log("Error", error);
-       });
-     console.log("OCR_backend : ", ocr_response);
-   };
+  const backendOCRExtractor = async () => {
+    const ocr_response = await fetch(
+      "https://cash-flowapp.herokuapp.com/api/detect-expenditure/",
+      {
+        //  mode: "no-cors",
+        method: "POST",
+        headers: {
+          "Content-Type": "multipart/form-data",
+          'Authorization': `Bearer ${localStorage.getItem("authToken")}`,
+        },
+        body: {
+          exp_pic: uploadFile[0],
+        },
+      }
+    )
+      .then((response) => {
+        console.log(response.json());
+      })
+      .then((data) => {
+        console.log("Success : ", data);
+      })
+      .then((error) => {
+        console.log("Error", error);
+      });
+    console.log("OCR_backend : ", ocr_response);
+  };
 
-const dummy_data = {
-  mer_name : "Humpty Dumpty ",
-  mer_address: "Mumbai"
-}
+  const dummy_data = {
+    mer_name: "Humpty Dumpty ",
+    mer_address: "Mumbai",
+  };
 
   const googleMapsExtractor = async () => {
     const maps_response = await fetch(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${dummy_data.mer_name+dummy_data.mer_address}&key=${process.env.GOOGLE_MAPS_API_KEY}` , {
-        method: 'GET',
-        mode: 'no-cors',
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${
+        dummy_data.mer_name + dummy_data.mer_address
+      }&key=${process.env.GOOGLE_MAPS_API_KEY}`,
+      {
+        method: "GET",
+        mode: "no-cors",
         headers: {
           "Content-type": "multipart/form-data",
-          "Authorization": `Bearer ${localStorage.getItem("authToken")}`,
+          'Authorization': `Bearer ${localStorage.getItem("authToken")}`,
         },
         params: {
           address: dummy_data.mer_name + dummy_data.mer_address,
@@ -90,16 +93,17 @@ const dummy_data = {
         },
       }
     )
-    .then(response => response.json())
-    .then((data) => {
-      console.log('Data : ', data);
-    })
-    .then((error) => {
-      console.log("Error : ", error);
-    });
+      .then((response) => {
+        console.log(response.json());
+      })
+      .then((data) => {
+        console.log("Data : ", data);
+      })
+      .then((error) => {
+        console.log("Error : ", error);
+      });
     console.log(maps_response);
-
-  }
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
