d9860c6b7e9b6ca3bfc64cd11a631e2152b3b6d1
Parth Cheulkar <pscheulkar12@gmail.com>
2022-01-19 16:34:02 +0530

    chalta hai kya dekhte

--- src/components/AddExpense/AddExpenseFormWithImg.js
+++ src/components/AddExpense/AddExpenseFormWithImg.js
@@ -8,7 +8,13 @@ export default function AddExpenseFormWithImg(props) {
   let history = useNavigate();
   const nano_id = "9f4d89f1-66cd-4db6-94fe-90d22fa3048c";
   const [uploadFile, setUploadFile] = useState([]);
-
+  let nano = {
+    mer_name: '',
+    mer_addr: '',
+    exp_amt: '',
+    exp_date: '',
+    cat: [],
+  }
   const uploadImageHandler = (acceptedFiles) => {
     setUploadFile(acceptedFiles);
     console.log(uploadFile);
@@ -29,13 +35,33 @@ export default function AddExpenseFormWithImg(props) {
       )
       .then((response) => {
         console.log(response.data);
+        response.data.result.prediction.map((pred) => {
+          if (pred.label === "Total_Amount" || pred.label === "Cash_Tender") {
+            nano.exp_amt = pred.ocr_text;
+          }
+
+          if (pred.label === "Merchant_Address") {
+            nano.mer_addr = pred.ocr_text;
+          }
+
+          if (pred.label === "Merchant_Name") {
+            nano.mer_name = pred.ocr_text;
+          }
+
+          if (pred.label === "Date") {
+            nano.exp_date = pred.ocr_text;
+          }
+        });
+        console.log('Nano: ',nano);
+        console.log("G Maps extractor Start");
+        // backendOCRExtractor();
+        googleMapsExtractor();
       });
   };
 
   const backendOCRExtractor = async () => {
    console.log(uploadFile[0]);
    var formData = new FormData();
-
    formData.append("exp_pic", uploadFile[0]);
    axios.post("https://cash-flowapp.herokuapp.com/api/detect-expenditure/",formData,
        {
@@ -47,17 +73,18 @@ export default function AddExpenseFormWithImg(props) {
      )
      .then((response) => {
        console.log(response.data);
+
      });
   };
 
-  const dummy_data = {
-    mer_name: "Humpty Dumpty",
-    mer_address: "Mumbai",
-  };
-
   const googleMapsExtractor = async () => {
-    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${dummy_data.mer_name + dummy_data.mer_address}&key=AIzaSyCoWpakOiniL2Ih_OZKsrOnf59_jT5y8D0`
-    ).then((response) => console.log(response.data));
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${nano.mer_name + " "+ nano.mer_addr}&key=AIzaSyCoWpakOiniL2Ih_OZKsrOnf59_jT5y8D0`
+    ).then((response) => {
+      console.log(response.data);
+      nano.cat = response.data.results.types;
+      console.log("Nano Final : ", nano);
+
+    });
   };
 
   const submitHandler = (e) => {
@@ -65,8 +92,8 @@ export default function AddExpenseFormWithImg(props) {
     console.log(uploadFile);
     console.log("data sent");
     nanoNetExtractor();
-    backendOCRExtractor();
-    googleMapsExtractor();
+    // backendOCRExtractor();
+    // googleMapsExtractor();
     setUploadFile([]);
     // history("/login");
   };
