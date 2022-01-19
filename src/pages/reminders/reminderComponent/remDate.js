b0ebc09ec8f31e2a0ce3be159b6f310112255143
Parth Cheulkar <pscheulkar12@gmail.com>
2022-01-19 11:12:37 +0530

    get reminders data

---
+++ src/pages/reminders/reminderComponent/remDate.js
@@ -0,0 +1,13 @@
+export default function RemDate(props) {
+  console.log(props.remDate);
+  const month = props.remDate.toLocaleString("en-US", { month: "long" }); //get month in word format
+  const day = props.remDate.toLocaleString("en-US", { day: "2-digit" }); // get 2-digit date nos
+  const year = props.remDate.getFullYear(); //get year eg. 2014
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+}
