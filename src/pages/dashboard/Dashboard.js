69c7ad7b4cf86ce614edc6d3cca3e319e5cb07c8
Shyren <shyren.more30@gmail.com>
2022-01-19 09:25:13 +0530

    add percentChangInExpenditure component

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import './Dashboard.css'
 import { Chart, ArcElement } from 'chart.js'
 import Heatmap from '../../components/Charts/Heatmap';
 import RecentTransactions from '../../components/Charts/RecentTransactions';
+import PercentChangeInExpenditure from '../../components/Charts/PercentChangeInExpenditure';
 Chart.register(ArcElement);
 
 // pass two arrays as props to legend component
@@ -22,9 +23,13 @@ const Dashboard = () => {
                         <Heatmap />
                     </div> 
                 </div>
-                <div className="row">
+                <div className="row d-flex justify-content-center align-items-center">
                     <div className="col-md-6 col-sm-12 col-xs-12 col-12 mx-2 my-2">
                         <RecentTransactions />
+                    </div>
+                    
+                    <div className="col-md-4 col-sm-12 col-xs-12 col-12 mx-2 my-2 ">
+                        <PercentChangeInExpenditure />
                     </div>
                     
                 </div>
